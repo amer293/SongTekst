7499cf66a578d5ee6a8b0f49591bb437b92e921b
amer293 <amerzahirovic@outlook.com>
2020-09-11 23:56:32 +0200

    init

---
+++ src/SongTekst.java
@@ -0,0 +1,92 @@
+public class SongTekst {
+    public static void main(String[] args)
+    {
+        // Too lazy to type the whole system.out.println()
+        var line = System.out;
+
+        // Define author + group
+
+
+        line.println(" ");
+        line.println("Title: Big Iron, Artist: Marty Robbins");
+        line.println(" ");
+        line.println(
+                "To the town of Agua Fria rode a stranger one fine day\n" +
+                        "Hardly spoke to folks around him, didn't have too much to say\n" +
+                        "No one dared to ask his business, no one dared to make a slip\n" +
+                        "The stranger there among them had a big iron on his hip\n" +
+                        "Big iron on his hip"
+        );
+        line.println(" ");
+        line.println(
+                "It was early in the morning when he rode into the town\n" +
+                        "He came riding from the south side, slowly lookin' all around\n" +
+                        "He's an outlaw loose and runnin'\", came a whisper from each lip\n" +
+                        "And he's here to do some business with a big iron on his hip\n" +
+                        "Big iron on his hip"
+        );
+        line.println(" ");
+        line.println(
+                "In this town there lived an outlaw by the name of Texas Red\n" +
+                        "Many men had tried to take him and that many men were dead\n" +
+                        "He was vicious and a killer, though a youth of twenty four\n" +
+                        "And the notches on his pistol numbered one and nineteen more\n" +
+                        "One and nineteen more"
+        );
+
+        line.println(" ");
+        line.println(
+                "Now the stranger started talkin' made it plain to folks around\n" +
+                        "Was an Arizona ranger, wouldn't be too long in town\n" +
+                        "He was here to take an outlaw back alive or maybe dead\n" +
+                        "And he said it didn't matter he was after Texas Red\n" +
+                        "After Texas Red"
+        );
+        line.println(" ");
+        line.println(
+                "Wasn't long before this story was relayed to Texas Red\n" +
+                        "But the outlaw didn't worry, men that tried before were dead\n" +
+                        "Twenty men had tried to take him, twenty men had made a slip\n" +
+                        "Twenty one would be the ranger with the big iron on his hip\n" +
+                        "Big iron on his hip"
+        );
+        line.println(" ");
+        line.println(
+                "Now the morning passed so quickly it was time for them to meet\n" +
+                        "It was twenty past eleven when they walked out in the street\n" +
+                        "Folks were watchin' from the windows\n" +
+                        "Every body held their breath\n" +
+                        "They knew this handsome ranger was about to meet his death\n" +
+                        "About to meet his death"
+        );
+        line.println(" ");
+        line.println(
+                "There was twenty feet between them\n" +
+                        "When they stopped to make their play\n" +
+                        "And the swiftness of the Ranger is still talked about today\n" +
+                        "Texas Red had not cleared leather for a bullet fairly ripped\n" +
+                        "And the ranger's aim was deadly, with the big iron on his hip\n" +
+                        "Big iron on his hip"
+        );
+        line.println(" ");
+        line.println(
+                "It was over in a moment and the folks had gathered 'round\n" +
+                        "There before them lay the body of the outlaw on the ground\n" +
+                        "Oh, he might have went on livin' but he made one fatal slip\n" +
+                        "When he tried to match the ranger with the big iron on his hip\n" +
+                        "Big iron on his hip"
+        );
+        line.println(" ");
+        line.println(
+                "Big iron, big iron\n" +
+                        "When he tried to match the ranger with the big iron on his hip\n" +
+                        "Big iron on his hip"
+        );
+        line.println(" ");
+
+
+
+    }
+}
+
+
